7cf553d3babe84247a79a4519f46e34e040e10da
ycq <1447589683@qq.com>
2022-04-27 10:23:38 +0800

    单例模式记录

---
+++ 第一部分/jsUtil.js
@@ -0,0 +1,10 @@
+// 单例
+var jsUtil = {
+    getSingle: function (func) {
+        var result;
+        return function (arguments) {
+            if(!result) result= func.apply(this,arguments);
+            return result;
+        }
+    }
+}
