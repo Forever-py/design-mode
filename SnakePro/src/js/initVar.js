c63bea020dda6f20781fb27fce042bde7f211c2e
ycq <1447589683@qq.com>
2022-05-06 09:41:35 +0800

    设计模式应用

---
+++ SnakePro/src/js/initVar.js
@@ -0,0 +1,34 @@
+// 游戏场景 宽度系数-每一行有多少个方块    高度系数 控制一共有多少行
+var XLEN = 30,
+    YLEN = 30;
+
+// 每个放款多宽
+var SQUAREWIDTH = 20;
+
+// 游戏场景的位置
+var BASE_X_POINT = 200,
+    BASE_Y_POINT = 200;
+
+// 定义 蛇移动的时间间隔
+var INTERVAL = 300;
+
+// 定义方块
+function Square(x, y, width, height, dom) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.viewContent = dom || document.createElement('div');
+}
+
+Square.prototype.touch = function () {
+    console.log('touch')
+}
+
+var Floor = tool.extends(Square); // 地板
+var Stone = tool.extends(Square);
+var Food = tool.single(Square); // 食物
+var SnakeHead = tool.single(Square); // 蛇头
+var SnakeBody = tool.single(Square); // 蛇身体
+var Ground = tool.single(Square); // 场景
+// var Game = tool.single(Square); // 游戏进程
---
+++ SnakePro/src/js/jsUtil.js
@@ -0,0 +1,32 @@
+var tool = {
+    inherit: function (target, origin) {
+        var temp = function () { };
+        temp.prototype = origin.prototype;
+        target.prototype = new temp();
+        target.prototype.constructor = target;
+    },
+    extends: function (origin) {
+        var result = function () {
+            // 私有属性
+            origin.apply(this, arguments)
+            return this;
+        };
+        // 原型
+        this.inherit(result, origin);
+        return result;
+    },
+    single: function (origin) { // 单例
+        var singleResult = (function () {
+            var instance;
+            return function () {
+                if (!instance) {
+                    origin && origin.apply(this, arguments);
+                    instance = this;
+                }
+                return instance;
+            };
+        })();
+        origin && this.inherit(singleResult, origin);
+        return singleResult;
+    }
+}
